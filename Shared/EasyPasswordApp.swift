081b9216ce661e2e83c2ca43baf1a7172f32cedb
Berkant Dursun <berkant.dursun@me.com>
2020-11-22 20:34:51 +0100

    Initial Commit

---
+++ Shared/EasyPasswordApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyPasswordApp.swift
+//  Shared
+//
+//  Created by Berkant Dursun on 22.11.20.
+//
+
+import SwiftUI
+
+@main
+struct EasyPasswordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
