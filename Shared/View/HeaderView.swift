0f16f5c8e278a030b71e9c926710109a8e017909
Berkant72 <berkant.dursun@me.com>
2023-06-20 20:54:19 +0200

    Updated animation modifier.

--- Shared/View/HeaderView.swift
+++ Shared/View/HeaderView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct HeaderView: View {
+    
     // MARK: - PROPERTIES
     
     @AppStorage("isDarkMode") private var isDarkMode: Bool =  false
@@ -18,37 +19,33 @@ struct HeaderView: View {
     // MARK: - BODY
     
     var body: some View {
-    
-            VStack {
-                
-                Image(systemName: "lock.rectangle")
-                    .foregroundColor(.orange)
-                    .font(.system(size: 60))
-                    .shadow(color: .orange.opacity(0.5), radius: 8, x: 4.0, y: 4.0)
-                    .scaleEffect(CGSize(width: animateTitle ? 1.5 : 0.2, height: animateTitle ? 1.5 : 0.2))
-                    .animation(Animation.easeIn.speed(0.4))
-                
-                
-                Text("\(title)")
-                    .foregroundColor(.orange)
-                    .font(.headline)
-                    .shadow(color: .orange.opacity(0.5), radius: 8, x: 4.0, y: 4.0)
-                    .offset(x: 0, y: animateTitle ? 40 : 0)
-                    .scaleEffect(CGSize(width: animateTitle ? 1.5 : 0.2, height: animateTitle ? 1.5 : 0.2))
-                    .animation(Animation.easeIn.speed(0.4))
-                
-                
-                
-            } //: VSTACK
-//            .frame(width: 300, height: 250, alignment: .center)
-            .onAppear(perform: {
-                animateTitle = true
-            })
-            .onDisappear(perform: {
-                animateTitle = false
-            })
-            .preferredColorScheme(isDarkMode ? .dark : .light)
-
+        
+        VStack {
+            
+            Image(systemName: "lock.rectangle")
+                .foregroundColor(.orange)
+                .font(.system(size: 60))
+                .shadow(color: .orange.opacity(0.5), radius: 8, x: 4.0, y: 4.0)
+                .scaleEffect(CGSize(width: animateTitle ? 1.5 : 0.2, height: animateTitle ? 1.5 : 0.2))
+                .animation(Animation.easeIn, value: speed_t(0.4))
+            
+            Text("\(title)")
+                .foregroundColor(.orange)
+                .font(.headline)
+                .shadow(color: .orange.opacity(0.5), radius: 8, x: 4.0, y: 4.0)
+                .offset(x: 0, y: animateTitle ? 40 : 0)
+                .scaleEffect(CGSize(width: animateTitle ? 1.5 : 0.2, height: animateTitle ? 1.5 : 0.2))
+                .animation(Animation.easeIn, value: speed_t(0.4))
+            
+        }
+        .onAppear(perform: {
+            animateTitle = true
+        })
+        .onDisappear(perform: {
+            animateTitle = false
+        })
+        .preferredColorScheme(isDarkMode ? .dark : .light)
+        
     }
 }
 
